42918834c3844d839601d33440101de26a99e78f
nestorcalvo <Nestor98112612483>
2021-07-16 23:41:12 -0500

    Sidebar template added

--- app.py
+++ app.py
@@ -3,17 +3,59 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 import dash
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 import dash_html_components as html
 import pandas as pd
 import plotly.express as px
 
-app = dash.Dash(__name__)
 
-app.layout = html.Div(children = [
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-    html.H1(children = 'Basic app')
-    
-])
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+
+sidebar = html.Div(
+    [
+        html.H2("Sidebar", className="display-4"),
+        html.Hr(),
+        html.H3(
+            "Admin", className="admin_name"
+        ),
+        dbc.Nav(
+            [
+                dbc.NavLink("Home", href="/", active="exact"),
+                dbc.NavLink("Boiler", href="/boiler", active="exact"),
+                dbc.NavLink("Statistics", href="/statistics", active="exact"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+content = html.Div(id="page-content", style=CONTENT_STYLE)
+
+
+app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
