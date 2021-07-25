279c8de2218c702698db7bdc3764fbd5a510be66
nestorcalvo <Nestor98112612483>
2021-07-24 21:41:47 -0500

    layout configuration added

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 import pandas as pd
 import plotly.express as px
+from dash.dependencies import Input, Output
 
 external_scripts =[
     {
@@ -16,7 +17,6 @@ external_scripts =[
     }
 ]
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP], external_scripts=external_scripts)
-
 
 sidebar = html.Div(
     [
@@ -100,26 +100,69 @@ sidebar = html.Div(
     className = "sidebar",
 )
 
-content = html.Div(
-    [
-        html.Div(
-            html.Img(
-                src = "/assets/images/C1_icon_1.png",
-                className = "corr-icon"
-            ),
-            className = "corr-icon-container"
+url_bar_and_content_div = html.Div([
+    dcc.Location(id='url', refresh=False),
+    sidebar,
+    html.Div(id='page-content', className = 'content')
+])
+
+
+layout_boiler = [
+    html.Div(children = [
+        html.Img(
+            src = "/assets/images/C1_icon_1.png",
+            className = "corr-icon"
         ),
         html.H1(
             "OPTIMIZATION OF THE STEAM BOILER OPERATION FOR BUENCAFE LIOFILIZADO DE COLOMBIA",
             className = "content-title"
-        )
-    ],
-    id="page-content", 
-    className = "content")
+        )],
+        className = "corr-icon-container"
+    ),
+
+]
 
 
-app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
+layout_statistics = [
+    html.Div(children = [
+        html.Img(
+            src = "/assets/images/C1_icon_1.png",
+            className = "corr-icon"
+        ),
+        html.H1(
+            "BOILER #6 OPTIMIZATION",
+            className = "content-title"
+        )],
+        className = "corr-icon-container"
+    ),
+]
 
+
+
+
+@app.callback(     
+    Output('page-content','children'),
+    Input('url','pathname')
+)
+def layout_selectio(pathname):
+    if pathname == '/boiler':
+        return layout_boiler
+    elif pathname == '/statistics':
+        return layout_statistics
+    else:
+        return html.Div()
+
+app.layout = url_bar_and_content_div
+
+app.validation_layout = html.Div([
+    url_bar_and_content_div,
+    sidebar,
+    layout_boiler,
+    layout_statistics,
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
+
