d8ebe7c8b7d3afcdedbca4b0cce45d426e239fdb
Nestor Calvo <nestor.calvo@udea.edu.co>
2021-07-31 22:19:09 -0500

    self added to variable s3

--- database_helper.py
+++ database_helper.py
@@ -7,13 +7,13 @@ class DatabaseHelper:
     """
     def __init__(self, db_name):
         self.db_name = db_name
-        s3 = boto3.client('s3')
+        self.s3 = boto3.client('s3')
 
     def list_files(self):
         """
         List of files in bucket
         """
-        my_bucket = s3.Bucket(self.db_name)
+        my_bucket = self.s3.Bucket(self.db_name)
         for my_bucket_object in my_bucket.objects.all():
             print(my_bucket_object)
 
@@ -21,7 +21,7 @@ class DatabaseHelper:
         """
         Read a csv from the S3 and store the data in a variable
         """
-        obj = s3.get_object(Bucket= self.db_name, Key= filename) 
+        obj = self.s3.get_object(Bucket= self.db_name, Key= filename) 
         # get object and file (key) from bucket
         initial_df = pd.read_csv(obj['Body']) # 'Body' is a key word
         return initial_df
