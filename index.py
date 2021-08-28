cf5c3a1129cc77af1a62d572f351ba4a9b39a4ff
Nestor Calvo <nestor.calvo@udea.edu.co>
2021-08-27 22:10:16 -0500

    sidebar basic layout with css styles

--- index.py
+++ index.py
@@ -21,105 +21,113 @@ sidebar = html.Div(
             src = "/assets/images/buencafe_icon_1.png",
             className = "sidebar-image"
         ),
-        dbc.Nav(
-            [
-                html.Div(
-                    children = [
-                        dbc.NavLink(
-                            [
-                                html.I(className = "fas fa-house-user"), "Home"
-                            ], 
-                            href="/apps/home", active="exact"
-                        )
-                    ],
-                    className = "nav-icon-link"
-                )
-            ],
-            vertical=True,
-            pills=True,
-        ),
-        html.Hr(),
-        
         html.Div([
-            html.H4("Analytics")
-        ]),
-        dbc.Nav(
-            [
-                html.Div(
-                    children = [
-                        dbc.NavLink(
-                            [
-                                html.I(className = "fas fa-faucet"), "Water"
-                            ], 
-                            href="/apps/water", active="exact"
-                        )
-                    ],
-                    className = "nav-icon-link"
-                ),
+            dbc.Nav(
+                [
+                    html.Div(
+                        children = [
+                            dbc.NavLink(
+                                [
+                                    html.I(className = "fas fa-house-user"), "Home"
+                                ], 
+                                href="/apps/home", active="exact"
+                            )
+                        ],
+                        className = "nav-icon-link"
+                    )
+                ],
+                vertical=True,
+                pills=True,
+                className = "item__home",
+            ),
+        ], className = "sidebar__section-1")
+        html.Div([
 
-                html.Div(
-                    children=[
-                        dbc.NavLink(
-                            [
-                                html.I(className = "fas fa-gas-pump"),"Fuel"
-                            ], 
-                            href="/apps/fuel", active="exact"
-                        ),
-                    ],
-                    className = "nav-icon-link"
-                ),
+            html.Hr(className = "sidebar__division-bar"),
+            
+            # html.Div([
+            #     html.H4("Analytics")
+            # ]),
+            dbc.Nav(
+                [
+                    html.Div(
+                        children = [
+                            dbc.NavLink(
+                                [
+                                    html.I(className = "fas fa-faucet"), "Steam"
+                                ], 
+                                href="/apps/water", active="exact"
+                            )
+                        ],
+                        className = "nav-icon-link item__steam"
+                    ),
 
-                html.Div(
-                    children = [
-                        dbc.NavLink(
-                            [
-                                html.I(className = "fas fa-percentage"), "Efficiency"
-                            ], 
-                            href="/apps/efficiency", active="exact"
-                        )
-                    ],
-                    className = "nav-icon-link"
+                    html.Div(
+                        children=[
+                            dbc.NavLink(
+                                [
+                                    html.I(className = "fas fa-gas-pump"),"Fuel"
+                                ], 
+                                href="/apps/fuel", active="exact"
+                            ),
+                        ],
+                        className = "nav-icon-link item__fuel"
+                    ),
+
+                    html.Div(
+                        children = [
+                            dbc.NavLink(
+                                [
+                                    html.I(className = "fas fa-percentage"), "Efficiency"
+                                ], 
+                                href="/apps/efficiency", active="exact"
+                            )
+                        ],
+                        className = "nav-icon-link item__eficiency"
+                    )
+                ],
+                vertical=True,
+                pills=True,
+            ),
+        ], className = "sidebar__section-2")
+        html.Div([
+            html.Hr(className = "sidebar__division-bar"),
+            dbc.Nav(
+                [
+                    html.Div(
+                        children = [
+                            dbc.NavLink(
+                                [
+                                    html.I(className = "fas fa-cogs"), "Settings"
+                                ], 
+                                href="/apps/settings", active="exact"
+                            )
+                        ],
+                        className = "nav-icon-link"
+                    ),
+
+                    html.Div(
+                        children=[
+                            dbc.NavLink(
+                                [
+                                    html.I(className = "far fa-address-card"),"About us"
+                                ], 
+                                href="/apps/aboutus", active="exact"
+                            ),
+                        ],
+                        className = "nav-icon-link"
+                    ),
+                ],
+                vertical=True,
+                pills=True,
+            ),
+            html.Div(
+                html.Img(
+                    src = "/assets/images/mintic_image.png",
+                    className = "mintic-image"
                 )
-            ],
-            vertical=True,
-            pills=True,
-        ),
-        html.Hr(),
-        dbc.Nav(
-            [
-                html.Div(
-                    children = [
-                        dbc.NavLink(
-                            [
-                                html.I(className = "fas fa-cogs"), "Settings"
-                            ], 
-                            href="/apps/settings", active="exact"
-                        )
-                    ],
-                    className = "nav-icon-link"
-                ),
-
-                html.Div(
-                    children=[
-                        dbc.NavLink(
-                            [
-                                html.I(className = "far fa-address-card"),"About us"
-                            ], 
-                            href="/apps/aboutus", active="exact"
-                        ),
-                    ],
-                    className = "nav-icon-link"
-                ),
-            ],
-            vertical=True,
-            pills=True,
-        ),
-        html.Div(
-            html.Img(
-                src = "/assets/images/mintic_image.png",
-                className = "mintic-image"
-            )
-        ),
+            ),
+        ], className = "sidebar__section-3"),
         
     ],
     className = "sidebar-wrapper",
